c2e362068d5658fbd9fcdc3f8cb705d4cb566c27
Wendy <s1141409@mail.yzu.edu.tw>
2026-03-30 09:20:39 +0800

    Update main.cpp

--- week1_leapyear/src/main.cpp
+++ week1_leapyear/src/main.cpp
@@ -6,7 +6,7 @@ int main()
     int n;
     cout << "Checking whether a year is leap year or not. Please input a year: ";
     while (cin >> n) {
-        if ((n % 4 == 0 ||n %400 == 0) && n %100 != 0)
+        if ((n % 4 == 0 && n %100 != 0)||n %400 == 0) 
             cout << n << " is a leap year." << endl;
         else
             cout << n << " is not a leap year." << endl;
