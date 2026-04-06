cec9ab0ab21aec90b3ba85cdebaf4c0cdc35d896
Wendy <s1141409@mail.yzu.edu.tw>
2026-04-06 10:54:42 +0800

    Update main.cpp

--- week3_Asterisk rhombus/src/while/main.cpp
+++ week3_Asterisk rhombus/src/while/main.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+     for(int j=i;j<n;j++)
+        cout<<" ";
+    if(i==1)
+        cout<<"*";
+    else
+        for(int k=1;k<2*i;k++ )
+            cout<<"*";
+    cout<<endl;
+  }
+  for(int i=n-1;i>=1;i--){
+     for(int j=i;j<n;j++)
+        cout<<" ";
+    if(i==1)
+        cout<<"*";
+    else
+        for(int k=1;k<2*i;k++ )
+            cout<<"*";
+    cout<<endl;
+  }
+ return 0;
+}
